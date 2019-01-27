b2f65ead9d6d476018ef77878cd616d4b96f0c9f
Rui Lopes <rui.lourenco.lopes@gmail.com>
2019-01-27 16:21:50 +0000

    Minor modifications

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,9 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
 var TDBay = artifacts.require("./TDBay.sol");
 var Design = artifacts.require("./Design.sol");
+var TDBayToken = artifacts.require("./TDBayToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
   deployer.deploy(TDBay);
   deployer.deploy(Design);
+  deployer.deploy(TDBayToken);
 };
